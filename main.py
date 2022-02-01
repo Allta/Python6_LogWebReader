15a2ef97a41a0d99cba207503e5d4fd5fe55c32c
Allta <francois.delrue@ynov.com>
2022-02-01 11:46:26 +0100

    add source

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-import os
+from flask import Flask, render_template
+from time import sleep
 
-for filename in os.listdir("exercice1"):
-  print(filename)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html.jinja')
+
+
+@app.route('/tail')
+def tail():
+	# Open log file
+	log_path="/opt/Python-tail/app.log"
+	# def get_lines_number()
+	# def generate_logs()	
+
+	# return flask reponse with generate_logs
+	return app.response_class(generate_logs(), mimetype='text/plain')
+
+app.run()
