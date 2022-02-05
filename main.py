f325bc348a8a56abbdfc2f79a3c72729c1fcca5d
Allta <51991304+Allta@users.noreply.github.com>
2022-02-05 11:14:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-from flask import Flask, render_template
+#from flask import Flask, render_template
 from time import sleep
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
-@app.route('/')
+#@app.route('/')
 def index():
 	return render_template('index.html.jinja')
 
-
-@app.route('/tail')
+# Utile pour l'export sur site internet
+""" il s'agit de la syntaxe pour créer une page sur le site flask """
+#@app.route('/tail')
 def tail():
-	# Open log file
+	# log file
 	log_path="/opt/Python-tail/app.log"
-	# def get_lines_number()
+	
+	# BONUS : def get_lines_number()
+		# Dans cette fonction il faut réussir à trouver le nombre de ligne du fichier et retourner ce nombre
 	# def generate_logs()	
-
+		# Fonction qui lit un fichier en temps réel: ouvrir le fichier, boucler en permanence sur les lignes et les afficher.
 	# return flask reponse with generate_logs
-	return app.response_class(generate_logs(), mimetype='text/plain')
+	#return app.response_class(generate_logs(), mimetype='text/plain')
 
-app.run()
+# Permet de lancer l'application Flask
+#app.run()
